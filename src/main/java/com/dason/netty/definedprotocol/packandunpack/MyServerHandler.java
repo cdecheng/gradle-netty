f899649c0c46f40a34fd90069380dbb3675931ef
Dason <dasonchan@outlook.com>
2020-05-03 18:21:33 +0800

    update 整理粘包拆包代码

---
+++ src/main/java/com/dason/netty/definedprotocol/packandunpack/MyServerHandler.java
@@ -0,0 +1,46 @@
+package com.dason.netty.definedprotocol.packandunpack;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.nio.charset.Charset;
+import java.util.UUID;
+
+//处理业务的handler
+public class MyServerHandler extends SimpleChannelInboundHandler<MessageProtocol> {
+    private int count;
+
+    /**
+     * 这里尽管是接收客户端传过来的数据，但是这里的数据是经过解码对象MyMessageDecoder进行解码
+     * 也就是将ByteBuf的数据转换成我们要的封装的对象，然后这样一来，我们每一个数据过来的都是一个
+     * MessageProtocol
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, MessageProtocol msg) throws Exception {
+
+        //接收到数据，并处理
+        int len = msg.getLen();
+        byte[] content = msg.getContent();
+        System.out.println("服务器接收到信息如下");
+        System.out.println("长度=" + len);
+        System.out.println("内容=" + new String(content, Charset.forName("utf-8")));
+        System.out.println("服务器接收到消息包数量=" + (++this.count));
+
+        //回复消息
+        String responseContent = UUID.randomUUID().toString();
+        int responseLen = responseContent.getBytes("utf-8").length;
+        byte[]  responseContent2 = responseContent.getBytes("utf-8");
+        //构建一个协议包
+        MessageProtocol messageProtocol = new MessageProtocol();
+        messageProtocol.setLen(responseLen);
+        messageProtocol.setContent(responseContent2);
+        ctx.writeAndFlush(messageProtocol);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        //cause.printStackTrace();
+        ctx.close();
+    }
+
+}
